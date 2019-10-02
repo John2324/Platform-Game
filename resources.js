ec4d01fcdb01973f7815703c4440dc7227aec61d
John <jmonte80@calstatela.edu>
2019-10-02 12:44:45 -0700

    Update resources

--- resources.js
+++ resources.js
@@ -9,6 +9,16 @@ var resources = [{
     src: "player.png"
   },
   {
+    name: "elementSprites",
+    type: "image",
+    src: "elementSprites.png"
+  },
+  {
+    name: "tileset",
+    type: "image",
+    src: "tileset.png"
+  },
+  {
     name: "level1",
     type: "tmx",
     src: "level1.tmx"
